12a86194f0ace52fbbed571b9d2a40263532f5fc
alex <alexhigueramartinez@gmail.com>
2025-05-20 01:24:58 +0200

    build: cpp00 ex01

--- cpp00/ex01/phonbook.hpp
+++ cpp00/ex01/phonbook.hpp
@@ -3,19 +3,27 @@
 /*                                                        :::      ::::::::   */
 /*   phonbook.hpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: alex <alex@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: columbux <columbux@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/03 13:02:58 by alex              #+#    #+#             */
-/*   Updated: 2024/10/03 13:05:08 by alex             ###   ########.fr       */
+/*   Updated: 2025/05/20 00:51:32 by columbux         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PHONEBOOK_HPP
-# define PHONEBOOK_HPP
+#pragma once
 
-class phonebook
-{
+#include "Contact.hpp"
+#include <iostream>
 
+class PhoneBook {
+private:
+	Contact	contacts[8];
+	int 	size;
+	int 	index;
+public:
+	PhoneBook() : size (0), index (0) {}
+	int 	get_size(void) const;
+	void	add_contact(std::string data[5]);
+	void	display_contact(int i) const;
+	void	display_phonebook(void) const;
 };
-
-#endif
