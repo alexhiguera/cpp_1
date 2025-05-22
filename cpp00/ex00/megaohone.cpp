eb6524c022160a9d304169fb6296a62387fcb317
alex <alexhigueramartinez@gmail.com>
2025-05-22 14:49:49 +0200

    update: colors and fix ex00

--- cpp00/ex00/megaohone.cpp
+++ cpp00/ex00/megaohone.cpp
@@ -3,30 +3,29 @@
 /*                                                        :::      ::::::::   */
 /*   megaohone.cpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: alex <alex@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: columbux <columbux@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/26 11:13:15 by alex              #+#    #+#             */
-/*   Updated: 2024/10/03 12:54:23 by alex             ###   ########.fr       */
+/*   Updated: 2025/05/22 14:45:12 by columbux         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <string>
 
-void	cppPrintMsg(const std::string& msg)
+int	main(int argc, const char **argv)
 {
-	for (size_t i = 0; i < msg.length(); i++)
-		std::cout << char(std::toupper(msg[i])) << std::endl;
-}
-
-int	main(int argc, char **argv)
-{
-	if (argc != 2) {
-		std::cout << "🚩 Insert only one argument!" << std::endl;
-		return (0);
+	if (argc > 1)
+	{
+		for (int i = 1; i < argc; i++)
+		{
+			std::string str(argv[i]);
+			for (size_t j = 0; j < str.length(); j++)
+				std::cout << static_cast<char>(std::toupper(str[j]));
+		}
 	}
-	for(int i = 1; i < argc; ++i)
-        cppPrintMsg(std::string(argv[i]));
+	else
+		std::cout << "🚩 Insert only one argument!" << std::endl;
 	std::cout << std::endl;
 	return (0);
 }
